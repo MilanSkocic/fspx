b5bb77b139a759b0f4c56d4c5d53f60f88014429
jalvesz <jose.alves@transvalor.com>
2024-11-06 22:23:38 +0100

    add example math

--- docs/example/example.f90
+++ docs/example/example.f90
@@ -3,6 +3,7 @@ module mod_example
     !> still some documentation
     !>
     !> yet more lines
+    !> \( ax^2 + bx + c = 0 \)
     implicit none
     private
     
@@ -16,6 +17,9 @@ contains
 
 subroutine my_sub(x,y)
 !> mysub docstrings
+!> \[
+!> x = \frac{-b \pm \sqrt{b^2 - 4ac}}{2a}
+!> \]
     real, intent(in) :: x !> input variable
     real, intent(inout) :: y !> output variable
 end subroutine
