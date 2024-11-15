4dd0113132a1301c37964a3d67f086755227d132
Milan Skocic <milan.skocic@icloud.com>
2024-11-15 06:46:02 +0100

    Initialize the docstring_character.

--- src/fspx/__init__.py
+++ src/fspx/__init__.py
@@ -7,7 +7,8 @@ def setup(app):
     Registers the AutoFortranDirective for automatically documenting Fortran code.
     """
     app.add_directive("autofortran", AutoFortranDirective)
-    
+    app.add_config_value("fspx_docstring_character", "!>", "!>")
+
     return {
         'version': '0.1.0',
         'parallel_read_safe': True,
